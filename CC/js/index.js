57b95296cf01fe17986a7a8f1a9be591dab6f010
XW <xuweiiam@gmail.com>
2014-06-26 17:02:08 +0800

    Update

--- CC/js/index.js
+++ CC/js/index.js
@@ -1,26 +1,26 @@
-window.onload = initAll;
-function initAll() {
-	login();
-	topLocation();
-}
-function topLocation() {
+$(function () {
+	$(".audio").mb_miniPlayer( {
+		width: 200,
+		inLine: false,
+		showRew: true,
+		showTime: true
+	});
+	$("a.item").mouseover(function() {
+		$(this).css({
+			"text-decoration": "underline",
+			"color": "red"
+		});
+	});
+	$("a.item").mouseout(function() {
+		$(this).css({
+			"text-decoration": "none",
+			"color": "#fffaf0"
+		});
+	});
+});
+$(function topLocation() {
 	if(top.location != self.location) {
 		top.location.replace(self.location);
-		//如果使用top.location = self.location;那么浏览器的back按钮就会失效
+		//濡傛灉浣跨敤top.location = self.location;閭ｄ箞娴忚鍣ㄧ殑back鎸夐挳灏变細澶辨晥
 	}
-}
-function login() {
-	if( document.getElementById ) {
-		document.getElementById("login").onclick = pop_up;
-		return false;
-	} else {
-		alert( "Your browser cannot support this script" );
-	}
-}
-function pop_up( evt ) {
-	if( evt ) {
-		//alert("Love!!!");
-	} else {
-		//alert("Hate!!!");
-	}
-}
+});
